92d0f649b2290606202b9ec6cdf53dea98c140d8
shamaz332 <shamazsaeed332@gmail.com>
2020-07-20 20:24:33 +0500

    deployment error resolved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./App.css";
 import useWebAnimations from "@wellyshen/use-web-animations";
 export default function App() {
@@ -69,12 +69,14 @@ export default function App() {
       <div>
         <img
           className="imgBg"
+          alt="pic"
           src="https://media.gettyimages.com/videos/grass-animation-video-id1132904944?s=640x640"
         />
         <div>
           <div ref={tree}>
             <img
               className="tree1"
+              alt="pic"
               src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Trees-PNG-Clipart/Tree_Transparent_Clipart_Picture.png?m=1434276625"
             />
           </div>
@@ -82,6 +84,7 @@ export default function App() {
           <div ref={treeTwo}>
             <img
               className="tree2"
+              alt="pic"
               src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Trees-PNG-Clipart/Tree_Transparent_Clipart_Picture.png?m=1434276625"
             />
           </div>
@@ -89,20 +92,22 @@ export default function App() {
             <img
               className="tree3"
               src="https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Trees-PNG-Clipart/Tree_Transparent_Clipart_Picture.png?m=1434276625"
+              alt="pic"
             />
           </div>
-
 
           <div className="lion" ref={horse}>
             <img
               className="imgSize"
               src="https://cdn.lowgif.com/full/a7cbb4c04b0592e9-running-lion-animated-gif-www-pixshark-com-images.gif"
+              alt="pic"
             />
           </div>
           <div className="goat" ref={lion}>
             <img
               className="horseimgSize"
               src="https://bestanimations.com/Animals/Mammals/Horses/animated-horse-gif-38.gif"
+              alt="pic"
             />
           </div>
         </div>
